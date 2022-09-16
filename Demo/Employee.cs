96b38baf2e8ed26197bf116ffc6a2ab06f0714cc
minal999 <minughadge555@gmail.com>
2022-09-16 16:23:46 +0530

    commit

---
+++ Demo/Employee.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Demo
+{
+    internal class Employee
+    {
+
+        protected int id;
+        private static int count = 0;
+        protected string name;
+        protected double bs, hra, da, pf, pt,total,netpaid;
+
+        //public Employee()
+        //{
+        //    name = "";
+        //    bs =0.0;
+        //    pt = 0.0;
+        //}
+        public Employee(string name,double bs,double pt)
+        {
+            count++;
+            id = count;
+            this.name = name;
+            this.bs = bs;
+            this.pt = pt;
+        }
+        public static int DisplayCount()
+        {
+            return count;
+        }
+        public virtual void CalculateSalary()
+        {
+            hra = bs * 0.40;
+            da = bs * 0.20;
+            pf = bs * 0.12;
+            total = bs + hra + da;
+            netpaid = total - (pf + pt);
+        }
+        public override string ToString()
+        {
+            return $"Employee Id : {id}\nEmployee name : {name}\nTotal salary : {total}\nNet paid : {netpaid}";
+        }
+        public class Manager : Employee
+        {
+            private double ta;
+            //public Manager() : base() // call constructor
+            //{
+            //    ta = 0.0;
+            //}
+            public Manager(string name, double bs, double pt, double ta) : base(name, bs, pt) // call constructor of base class
+            {
+                this.ta = ta;
+            }
+            public override void CalculateSalary()
+            {
+                hra = bs * 0.40;
+                da = bs * 0.20;
+                pf = bs * 0.12;
+                total = bs + hra + da + ta;
+                netpaid = total - (pf + pt);
+            }
+            public override string ToString()
+            {
+                return $"Manager Id : {id}\nManager name : {name}\nTotal salary : {total}\nNet paid : {netpaid}";
+            }
+        }
+        public class SalesManager : Employee
+        {
+            private double comm,bonus;
+            //public Manager() : base() // call constructor
+            //{
+            //    ta = 0.0;
+            //}
+            public SalesManager(string name, double bs, double pt, double comm, double bonus) : base(name, bs, pt) // call constructor of base class
+            {
+                this.comm = comm;
+                this.bonus = bonus;
+            }
+            public override void CalculateSalary()
+            {
+                hra = bs * 0.40;
+                da = bs * 0.20;
+                pf = bs * 0.12;
+                total = bs + hra + da + comm+bonus;
+                netpaid = total - (pf + pt);
+            }
+            public override string ToString()
+            {
+                return $"SalesManager Id : {id}\nSalesManager name : {name}\ntotal salary : {total}\nNet paid : {netpaid}";
+            }
+        }
+
+
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Demo.Employee;
 
 namespace Demo
 {
@@ -10,6 +11,23 @@ namespace Demo
     {
         static void Main(string[] args)
         {
+            //Manager m1 = new Manager("managar1", 45000, 250, 5000);
+            //m1.CalculateSalary();
+            //Console.WriteLine(m1);
+
+            //Employee e1 = new Manager("managar2", 43000, 250, 5000);
+            //// association of method with object does at run time
+            //e1.CalculateSalary(); // runtime polymorphism
+            //Console.WriteLine(e1);
+
+            //Employee e2 = new Employee("emp1", 34000, 200);
+            //e2.CalculateSalary();
+            //Console.WriteLine(e2);
+
+            SalesManager sm1 = new SalesManager("jassi", 50000, 300, 200, 5000);
+            sm1.CalculateSalary();
+            Console.WriteLine(sm1);
+
         }
     }
 }
