b442592e25973316e5aede15e89989e6adb7d5bb
minal999 <minughadge555@gmail.com>
2022-09-16 17:52:23 +0530

    commit

---
+++ Abstract/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c1 = new Circle(5);
+            c1.Area();
+            Console.WriteLine(c1);
+
+
+            Rectangle r1 = new Rectangle(12, 5);
+            r1.Area();
+            Console.WriteLine(r1);
+        }
+    }
+}
---
+++ Abstract/Shape.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    abstract public class Shape
+    {
+        public abstract void Area();
+    }
+    public class Circle : Shape
+    {
+
+        private int radius;
+        private double result;
+        public Circle(int radius)
+        {
+            this.radius = radius;
+        }
+
+        public override void Area()
+        {
+            result = 3.14 * radius * radius;
+        }
+        public override string ToString()
+        {
+            return $"Area of Circle is {result}";
+        }
+    }
+    // homework
+    public class Rectangle : Shape
+    {
+        private double l,b;
+        private double result;
+        public Rectangle(double l,double b)
+        {
+            this.l = l;
+            this.b = b;
+        }
+
+        public override void Area()
+        {
+            result =l*b;
+        }
+        public override string ToString()
+        {
+            return $"Area of Rectangle is {result}";
+        }
+    }
+
+}
